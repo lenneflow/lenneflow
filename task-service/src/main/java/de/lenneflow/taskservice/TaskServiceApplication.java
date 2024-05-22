6580869b3c59af0699893d876bcb6bdd7feaeb0c
Idrissa Ganemtore <dev@ganemtore.net>
2024-05-22 16:31:18 -0400

    added basic queue implementations

--- task-service/src/main/java/de/lenneflow/taskservice/TaskServiceApplication.java
+++ task-service/src/main/java/de/lenneflow/taskservice/TaskServiceApplication.java
@@ -1,13 +1,35 @@
 package de.lenneflow.taskservice;
 
+import org.springframework.amqp.AmqpException;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
+import org.springframework.amqp.rabbit.connection.Connection;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.connection.ConnectionListener;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class TaskServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskServiceApplication.class, args);
+    }
+
+    @Bean
+    public ConnectionFactory connectionFactory() {
+        CachingConnectionFactory connectionFactory = new CachingConnectionFactory();
+        connectionFactory.setAddresses("address");
+        connectionFactory.setUsername("username");
+        connectionFactory.setPassword("password");
+        return connectionFactory;
+    }
+
+    @Bean
+    public AmqpAdmin amqpAdmin() {
+        return new RabbitAdmin(connectionFactory());
     }
 
 }
