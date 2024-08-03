05943d21caba10d167d6256b3b3ae224f94fe3b7
Idrissa GANEMTORE <dev@ganemtore.net>
2024-08-03 13:49:15 -0400

    updated

--- orchestration-service/src/main/java/de/lenneflow/orchestrationservice/controller/DefaultRestController.java
+++ orchestration-service/src/main/java/de/lenneflow/orchestrationservice/controller/DefaultRestController.java
@@ -1,7 +1,9 @@
 package de.lenneflow.orchestrationservice.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class DefaultRestController {
 
     @GetMapping("/")
