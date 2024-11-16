17a300b2eb8198008266d15cdf7fa78c712ef786
Idrissa Ganemtore <dev@ganemtore.net>
2024-11-16 11:59:05 -0500

    update

--- function-service/src/main/java/de/lenneflow/functionservice/controller/FunctionController.java
+++ function-service/src/main/java/de/lenneflow/functionservice/controller/FunctionController.java
@@ -16,11 +16,9 @@ import de.lenneflow.functionservice.repository.JsonSchemaRepository;
 import de.lenneflow.functionservice.util.ObjectMapper;
 import de.lenneflow.functionservice.util.Validator;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
